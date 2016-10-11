edceffdf34e6bda17bb99c08caadd879ea74ab3f
Robert Zehnder <robzcodes@gmail.com>
2016-10-11 14:56:14 -0400

    Completes exercise, not last two bonus

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-var names = ["Edward", "Barefoot Contessa", "Alucard", "Shania", "Bobby Brown"];
+$( document ).ready(function() {
+  var names = ["Edward", "Barefoot Contessa", "Alucard", "Shania", "Bobby Brown"];
 
-var article = {
-  header: {
-    title: "Gibberish",
-    byline: "by Chaos Octopus"
-  },
-  editors: ["Daniella Stanley", "Stan Danley"],
-  body: {
-    imageUrl: "http://www.placebear.com/333/333",
-    text: 'Shark far unsafely some hello a over hey overabundant rarely amiable so as surreptitious ceaseless busted flabbily as some quail jeepers some vibrantly house the wow wow hello far. Hey this hey concomitant when horrendous gull within far alas forgave thanks after poignantly tritely flamingo rarely one sullenly jocose.'
+  var article = {
+    header: {
+      title: "Gibberish",
+      byline: "by Chaos Octopus"
+    },
+    editors: ["Daniella Stanley", "Stan Danley"],
+    body: {
+      imageUrl: "http://www.placebear.com/333/333",
+      text: 'Shark far unsafely some hello a over hey overabundant rarely amiable so as surreptitious ceaseless busted flabbily as some quail jeepers some vibrantly house the wow wow hello far. Hey this hey concomitant when horrendous gull within far alas forgave thanks after poignantly tritely flamingo rarely one sullenly jocose.'
+    }
   }
-}
+
+//Question 2
+  for (i=0;i<names.length;i++) {
+    console.log(names[i]);
+  }
+
+//Question 3
+$( "div" ).each(function( index ) {
+  $( this ).text();
+});
+
+//Question 4
+$( "div" ).each(function(  ) {
+  $( this ).text("Rob");
+});
+
+//Question 5
+$( "div" ).each(function( index ) {
+  $( this ).text(names[index]);
+});
+
+//Question 6
+var mainText = $("article").text();
+$("main").text(mainText);
+
+//Bonus 1
+var title = "Title";
+$("h3").text(title);
+
+});
